4680d20816728a501b823efc070d8bba7b0f7379
battedaniel <battedaniel@a125a394-e1eb-b9d1-3f7b-3fa5d3116dec>
2010-12-09 03:11:32 +0000

    revisar plan

--- src/viviendas/gui/Plan/CtrlPlan.java
+++ src/viviendas/gui/Plan/CtrlPlan.java
@@ -2,10 +2,10 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package viviendas.gui.Plan;
 
 import javax.swing.JDesktopPane;
+import viviendas.entidades.vivienda.TipoPlan;
 import viviendas.modulos.Plan.GestorPlan;
 
 /**
@@ -13,6 +13,7 @@ import viviendas.modulos.Plan.GestorPlan;
  * @author Admin
  */
 public class CtrlPlan {
+
     private IUPlan _pantalla;
     private GestorPlan _gestor;
 
@@ -21,8 +22,21 @@ public class CtrlPlan {
         //acciones...
         _pantalla.setVisible(true);
         desktop.add(_pantalla);
+        _pantalla.getBtnAceptar().addActionListener(new java.awt.event.ActionListener() {
+
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cargarNuevoPlan();
+            }
+        });
     }
 
-
-
+    public void cargarNuevoPlan() {
+        DtoNuevoPlan dto = new DtoNuevoPlan();
+        dto.setAños(((Number)_pantalla.getSpinAños().getValue()).intValue());
+        dto.setCantidadViviendas(((Number)_pantalla.getFormatedViviendas().getValue()).intValue());
+        dto.setNombre(_pantalla.getTexNombre().getText());
+        dto.setProvincias(((ModeloProvincia)_pantalla.getTabProvinciasSeleccionadas().getModel()).getList());
+        dto.setTipo((TipoPlan)_pantalla.getComTipoPlan().getSelectedItem());
+        _gestor.crearNuevoPlan(dto);
+    }
 }
--- src/viviendas/modulos/Plan/GestorPlan.java
+++ src/viviendas/modulos/Plan/GestorPlan.java
@@ -5,10 +5,47 @@
 
 package viviendas.modulos.Plan;
 
+import java.util.ArrayList;
+import java.util.List;
+import viviendas.entidades.vivienda.AñoPlan;
+import viviendas.entidades.vivienda.DistribucionCiudad;
+import viviendas.entidades.vivienda.DistribucionProvincial;
+import viviendas.entidades.vivienda.Plan;
+import viviendas.entidades.vivienda.Provincia;
+import viviendas.gui.Plan.DtoNuevoPlan;
+
+
 /**
  *
  * @author Admin
  */
 public class GestorPlan {
+
+    public void crearNuevoPlan(DtoNuevoPlan dto) {
+        int cantidadAños = dto.getAños() - 2010;
+        int año = 2010;
+        Plan plan = new Plan();
+        plan.setAñosPlan(cantidadAños);
+        plan.setTipoPlan(dto.getTipo());
+        plan.setNombre(dto.getNombre());
+        plan.setNumeroViviendas(cantidadAños * dto.getCantidadViviendas());
+        List<AñoPlan> añosPlan = new ArrayList<AñoPlan>();
+        List<DistribucionProvincial> distribucionesProvinciales = new ArrayList<DistribucionProvincial>();
+        for (int i = 0; i < cantidadAños; i++) {
+            AñoPlan añoPlan = new AñoPlan();
+            añoPlan.setAño(año++);
+            añoPlan.setCantViviendasAño(dto.getCantidadViviendas());
+            añoPlan.setPlan(plan);
+            for (Provincia provincia : dto.getProvincias()) {
+                DistribucionProvincial distribucionProvincial = new DistribucionProvincial();
+                distribucionProvincial.setAñoPlan(añoPlan);
+                distribucionProvincial.setProvincia(provincia);
+                distribucionProvincial.setPorcentajeDistribucion(Double.NaN);
+                distribucionesProvinciales.add(distribucionProvincial);
+            }
+            añoPlan.setListaDistribucionProvincial(distribucionesProvinciales);
+        }
+        plan.setListaAñoPlan(añosPlan);
+    }
 
 }
