c24586479ce70475c2e24e5a28fda80f6d339d7f
battedaniel <battedaniel@a125a394-e1eb-b9d1-3f7b-3fa5d3116dec>
2010-12-09 15:37:23 +0000

    Persistencia: Intermediarios.

---
+++ src/viviendas/persistencia/IntermediarioTipoPlan.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viviendas.persistencia;
+
+import viviendas.entidades.vivienda.TipoPlan;
+
+/**
+ *
+ * @author batte
+ */
+public class IntermediarioTipoPlan extends Intermediario<TipoPlan> {
+
+    public IntermediarioTipoPlan() {
+        super("TipoPlan");
+    }
+}
