0fea09fb8c321718a33adcf417a7693ec0de1b44
e.maximiliano.marotta@gmail.com <e.maximiliano.marotta@gmail.com@a125a394-e1eb-b9d1-3f7b-3fa5d3116dec>
2010-12-09 14:38:17 +0000

    IntermediarioOperatoria

---
+++ branches/Viviendas/src/viviendas/persistencia/IntermediarioOperatoria.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package viviendas.persistencia;
+
+import viviendas.entidades.vivienda.Operatoria;
+
+/**
+ *
+ * @author Maximiliano.
+ */
+public class IntermediarioOperatoria extends Intermediario<Operatoria>{
+
+    public IntermediarioOperatoria() {
+        super("Operatoria");
+    }
+
+}
