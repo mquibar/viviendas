676398ed2b17ca43c42f9ad7f05b0c382448b507
e.maximiliano.marotta@gmail.com <e.maximiliano.marotta@gmail.com@a125a394-e1eb-b9d1-3f7b-3fa5d3116dec>
2010-12-17 16:50:29 +0000

    Nuevos parametros y reportes

---
+++ branches/Viviendas/src/viviendas/modulos/usosFondos/GestorUsosFondos.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package viviendas.modulos.usosFondos;
+
+/**
+ *
+ * @author Maximiliano.
+ */
+public class GestorUsosFondos {
+    public static final String NOMBRE_PARAM_USOSFONDOS = "USOSFONDOS_";
+
+}
