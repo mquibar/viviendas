ff0592ad5e4dc6875ce5a0aa573144f8d4b841bb
negromanuel.quibar@gmail.com <negromanuel.quibar@gmail.com@a125a394-e1eb-b9d1-3f7b-3fa5d3116dec>
2010-12-27 12:33:42 +0000

    Negro: 15/12 ---> Inversiones y valor de la inversion

--- src/viviendas/gui/Plan/modificar/CtrlGrande.java
+++ src/viviendas/gui/Plan/modificar/CtrlGrande.java
@@ -7,9 +7,7 @@ package viviendas.gui.Plan.modificar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyVetoException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.swing.JDesktopPane;
+import javax.swing.JPanel;
 import viviendas.gui.financiacion.crear.CtrlCrearFinanciacion;
 import viviendas.gui.sistema.CtrlPrincipal;
 import viviendas.modulos.Plan.modificar.GestorModificarPlan;
@@ -30,6 +28,7 @@ public class CtrlGrande {
         _pantalla = new IUModificarPlanNew();
         CtrlPrincipal.getInstance().getDesktopPane().add(_pantalla);
         _controlModificar = new ctrlModificarPlanNew(gestor, _pantalla);
+        agregarPanel(_controlModificar.getPanel());
         try {
             _pantalla.setMaximum(true);
         } catch (PropertyVetoException ex) {
@@ -45,9 +44,31 @@ public class CtrlGrande {
         _pantalla.toFront();
     }
 
+    private void agregarPanel(JPanel panel){
+        javax.swing.GroupLayout _pnlCentralLayout = new javax.swing.GroupLayout(_pantalla.getPnlCentral());
+        _pantalla.getPnlCentral().setLayout(_pnlCentralLayout);
+        _pnlCentralLayout.setHorizontalGroup(
+            _pnlCentralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, _pnlCentralLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, 1820, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        _pnlCentralLayout.setVerticalGroup(
+            _pnlCentralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(_pnlCentralLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, 810, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+    }
+
     void pressAddFinanciacionButton(){
         _controlModificar.desactivar();
-        if(_controlFinanciacion!=null)
+        if(_controlFinanciacion!=null){
             _controlFinanciacion = new CtrlCrearFinanciacion(_controlModificar.getDistOperatoriaSeleccionada(), null);
+            //agregarPanel(_controlFinanciacion.getPanel);
+        }
+        //_controlFinanciacion.activar();
     }
 }
