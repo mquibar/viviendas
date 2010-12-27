3b5a3f9f333f44281e2092f6184c9b8304c9344f
negromanuel.quibar@gmail.com <negromanuel.quibar@gmail.com@a125a394-e1eb-b9d1-3f7b-3fa5d3116dec>
2010-12-27 16:06:57 +0000

    Negro: 15/12 ---> Inversiones y valor de la inversion

---
+++ branches/Viviendas/src/viviendas/gui/Plan/modificar/CtrlModificarInversion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package viviendas.gui.Plan.modificar;
+
+/**
+ *
+ * @author desarrollo
+ */
+public class CtrlModificarInversion {
+    private PanelInversionPlan _panel;
+    private IUModificarPlanNew _pantalla;
+
+    public CtrlModificarInversion(IUModificarPlanNew pantalla) {
+        this._pantalla = pantalla;
+    }
+
+
+
+}
