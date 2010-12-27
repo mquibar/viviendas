16646b44529ea9f08fac0f39df138e4559fd7ac8
negromanuel.quibar@gmail.com <negromanuel.quibar@gmail.com@a125a394-e1eb-b9d1-3f7b-3fa5d3116dec>
2010-12-27 12:27:56 +0000

    Negro: 15/12 ---> Inversiones y valor de la inversion

--- branches/Viviendas/src/viviendas/gui/Plan/modificar/CtrlGrande.java
+++ branches/Viviendas/src/viviendas/gui/Plan/modificar/CtrlGrande.java
@@ -4,10 +4,14 @@
  */
 package viviendas.gui.Plan.modificar;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyVetoException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JDesktopPane;
+import viviendas.gui.financiacion.crear.CtrlCrearFinanciacion;
+import viviendas.gui.sistema.CtrlPrincipal;
 import viviendas.modulos.Plan.modificar.GestorModificarPlan;
 
 /**
@@ -18,19 +22,32 @@ public class CtrlGrande {
 
     private IUModificarPlanNew _pantalla;
     private ctrlModificarPlanNew _controlModificar;
+    private CtrlCrearFinanciacion _controlFinanciacion;
     private GestorModificarPlan _gestor;
 
-    public CtrlGrande(GestorModificarPlan gestor, JDesktopPane panel) {
+    public CtrlGrande(GestorModificarPlan gestor) {
         _gestor = gestor;
         _pantalla = new IUModificarPlanNew();
-        panel.add(_pantalla);
+        CtrlPrincipal.getInstance().getDesktopPane().add(_pantalla);
         _controlModificar = new ctrlModificarPlanNew(gestor, _pantalla);
         try {
             _pantalla.setMaximum(true);
         } catch (PropertyVetoException ex) {
-            Logger.getLogger(CtrlGrande.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println(ex);
         }
+        _pantalla.getBtnAddFinanciacion().addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                pressAddFinanciacionButton();
+            }
+        });
         _pantalla.setVisible(true);
         _pantalla.toFront();
+    }
+
+    void pressAddFinanciacionButton(){
+        _controlModificar.desactivar();
+        if(_controlFinanciacion!=null)
+            _controlFinanciacion = new CtrlCrearFinanciacion(_controlModificar.getDistOperatoriaSeleccionada(), null);
     }
 }
