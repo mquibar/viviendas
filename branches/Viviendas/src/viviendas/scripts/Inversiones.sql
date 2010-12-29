0fb882408f042632401698af3889ea55b3e0bc48
negromanuel.quibar@gmail.com <negromanuel.quibar@gmail.com@a125a394-e1eb-b9d1-3f7b-3fa5d3116dec>
2010-12-29 15:58:11 +0000

    Negro: 15/12 ---> Actualizaciones de Inversiones finalizada + look and Feel

--- branches/Viviendas/src/viviendas/scripts/Inversiones.sql
+++ branches/Viviendas/src/viviendas/scripts/Inversiones.sql
@@ -6,23 +6,23 @@ INSERT INTO viviendas.Inversion (DTYPE, totalInversion, ciudad_id)
 	VALUES ('IPlan', 100000.0, 3);
 
 
-INSERT INTO viviendas.ValorInversion (valorInversion, financio_id, inversion_id)
+INSERT INTO viviendas.ValorInversion (importe, financio_id, inversion_id)
 	VALUES (30000.0, 1, 1);
-INSERT INTO viviendas.ValorInversion (valorInversion, financio_id, inversion_id)
+INSERT INTO viviendas.ValorInversion (importe, financio_id, inversion_id)
 	VALUES (30000.0, 2, 1);
-INSERT INTO viviendas.ValorInversion (valorInversion, financio_id, inversion_id)
+INSERT INTO viviendas.ValorInversion (importe, financio_id, inversion_id)
 	VALUES (40000.0, 3, 1);
-INSERT INTO viviendas.ValorInversion (valorInversion, financio_id, inversion_id)
+INSERT INTO viviendas.ValorInversion (importe, financio_id, inversion_id)
 	VALUES (30000.0, 1, 2);
-INSERT INTO viviendas.ValorInversion (valorInversion, financio_id, inversion_id)
+INSERT INTO viviendas.ValorInversion (importe, financio_id, inversion_id)
 	VALUES (40000.0, 2, 2);
-INSERT INTO viviendas.ValorInversion (valorInversion, financio_id, inversion_id)
+INSERT INTO viviendas.ValorInversion (importe, financio_id, inversion_id)
 	VALUES (30000.0, 3, 2);
-INSERT INTO viviendas.ValorInversion (valorInversion, financio_id, inversion_id)
+INSERT INTO viviendas.ValorInversion (importe, financio_id, inversion_id)
 	VALUES (40000.0, 1, 3);
-INSERT INTO viviendas.ValorInversion (valorInversion, financio_id, inversion_id)
+INSERT INTO viviendas.ValorInversion (importe, financio_id, inversion_id)
 	VALUES (30000.0, 2, 3);
-INSERT INTO viviendas.ValorInversion (valorInversion, financio_id, inversion_id)
+INSERT INTO viviendas.ValorInversion (importe, financio_id, inversion_id)
 	VALUES (30000.0, 3, 3);
 
 INSERT INTO viviendas.Plan_Inversion (Plan_id, listaInversion_id)
